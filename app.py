5515efd392e51d457bc261395ac414c210deb2bf
arjaz <arjaz@protonmail.com>
2020-04-25 00:05:57 +0300

    Fix requirements and add link validation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from scraper import scrape_profile
 
 app = Flask(__name__)
@@ -11,10 +11,26 @@ def index():
 
 @app.route('/scrape', methods=['POST', 'GET'])
 def scrape():
-    userlink = request.form['userlink']
-    # TODO: validate link
-    repos = scrape_profile(userlink)
-    return render_template('scraped.html', repos=repos)
+    def link(url):
+        import re
+        regex = re.compile(
+            r'^(?:http)s?://'  # http:// or https://
+            r'(github\.com)|'  #domain...
+            r'(?::\d+)?'  # optional port
+            r'(?:/?|[/?]\S+)$',
+            re.IGNORECASE)
+        return re.match(regex, url) is not None
+
+    if request.method == 'POST':
+        userlink = request.form['userlink']
+        if not link(userlink):
+            actual_link = f'https://github.com/{userlink}'
+        else:
+            actual_link = userlink
+        repos = scrape_profile(actual_link)
+        return render_template('scraped.html', repos=repos)
+    else:
+        return redirect('/')
 
 
 if __name__ == '__main__':
