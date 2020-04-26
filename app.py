74633552f9c5879dd35b75c222d9b6919db1460d
arjaz <arjaz@protonmail.com>
2020-04-27 00:55:34 +0300

    Add almost full database integration and api

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-from models import Repo
+from models import Repo, User
 
-# TODO: add api with jsonify
+# TODO: Add caching
 
 
 @app.route('/')
@@ -19,11 +19,17 @@ def index():
     return render_template('index.html')
 
 
-# TODO: add user specification
-@app.route('/repositories')
-def repositories():
-    return "TODO"
-    pass
+@app.route('/api/user/<id_>')
+def get_user(id_):
+    try:
+        user = User.query.filter_by(id=id_).first()
+        repos = [repo.serialize() for repo in user.repos]
+        print(type(repos))
+        print(repos)
+
+        return jsonify(repos)
+    except Exception as e:
+        return str(e)
 
 
 @app.route('/api/repository/<id_>')
@@ -37,37 +43,60 @@ def get_repo(id_):
 
 @app.route('/scrape', methods=['POST'])
 def scrape():
+    # Check link
     def link(url):
         import re
         regex = re.compile(
             r'^(?:http)s?://'  # http:// or https://
-            r'(github\.com)|'  #domain...
-            r'(?::\d+)?'  # optional port
+            r'(github\.com)|'  # domain
             r'(?:/?|[/?]\S+)$',
             re.IGNORECASE)
         return re.match(regex, url) is not None
 
-    userlink = request.form['userlink']
+    # Userlink and username extraction
+    # TODO: add validation
+    userlink = request.form.get('userlink')
     if not link(userlink):
-        # TODO: Add name validation
         actual_link = f'https://github.com/{userlink}'
+        username = userlink
     else:
         actual_link = userlink
+        username = list(filter(lambda x: len(x) > 0,
+                               actual_link.split('/')))[-1]
+
+    # Scraping
     repos = scrape_profile(actual_link)
-    # TODO redirect to user profile with all his repositories
+
+    # Check user
+    user = User.query.filter_by(name=username).first()
+    if user is None:
+        try:
+            user = User(name=username, link=f'https://github.com/{username}')
+            db.session.add(user)
+            db.session.commit()
+        except Exception as e:
+            return str(e)
+
+    # If repo exists, do nothing TODO: update
+    # If repo doesn't exist, add it
     for repo in repos:
         try:
             name = repo['name']
             lang = repo['lang']
             description = repo['description'] if repo['description'] else ''
-            obj = Repo(name=name, lang=lang, description=description)
 
-            db.session.add(obj)
-            db.session.commit()
-            return redirect(f'/api/repository/{obj.id}')
+            registered = Repo.query.filter_by(name=name).first()
+            if registered is None:
+                obj = Repo(name=name,
+                           lang=lang,
+                           description=description,
+                           user_id=user.id)
+
+                db.session.add(obj)
+                db.session.commit()
         except Exception as e:
             return str(e)
-    return redirect('/')
+    return redirect(f'/api/user/{user.id}')
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -4,17 +4,20 @@ from app import db
 class Repo(db.Model):
     __tablename__ = 'repos'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    lang = db.Column(db.String())
-    description = db.Column(db.String())
+    name = db.Column(db.String(255), nullable=False)
+    lang = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('repos', lazy=True))
 
-    def __init__(self, name, lang, description):
+    def __init__(self, name, lang, description, user_id):
         self.name = name
         self.lang = lang
         self.description = description
+        self.user_id = user_id
 
     def __repr__(self):
-        return f'<id: {self.id}>'
+        return f'<repo: {self.id}>'
 
     def serialize(self):
         return {
@@ -22,3 +25,20 @@ class Repo(db.Model):
             'lang': self.lang,
             'description': self.description,
         }
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True, nullable=False)
+    link = db.Column(db.String(), unique=True, nullable=False)
+
+    def __init__(self, name, link):
+        self.name = name
+        self.link = link
+
+    def __repr__(self):
+        return f'<user: {self.id}>'
+
+    def serialize(self):
+        return {'name': self.name, 'link': self.link}
